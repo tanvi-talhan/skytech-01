72ad488605867f0310048f67865fd2b9e2e96172
Radshu21 <tanvitalhan@gmail.com>
2024-07-26 12:05:51 +0530

    "Carousel added"

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,15 @@
+//rafe(const boiler print)
+//rfce (function boiler print)
+
+import React from 'react'
+
+const Carousel = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Carousel
+
